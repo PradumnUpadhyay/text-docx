2d2fa9cdc9fe2fbefc6bb66e2b213e8957fccb14
Pradumn Upadhyay <pradumn.upadhyay1@gmail.com>
2020-11-30 17:04:50 +0530

    Update index.js
    Pytesseract changes

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const express=require('express')
 const app=express()
 
 // Utilities
-let doc=null; let fn=""
-const port= process.env.PORT
+let doc=null,pytessDocx=null; let fn="",filename="";
+const port= process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// const user={}
 
-// Function to write on different lines
+// For writing paragraph
 function paraWrite(para) {
     var li=para.split("\n")
     console.log(li)
@@ -24,12 +23,13 @@ function paraWrite(para) {
     return para;
 }
 
+
 // Routes
 app.get('/',(req,res)=>{
     res.send("Are you lost baby girl?")
 })
 
-// POST-download
+// OCR text docx
 app.post('/download',(req,res)=>{
     // Extract string from req body
     console.log("recieved Request")
@@ -62,7 +62,7 @@ catch(err) {
 }
 })// end of request
 
-// GET-download
+// OCR text docx
 app.get('/download', async (req,res)=>{
     try {
         console.log('request from device for docx')
@@ -73,6 +73,55 @@ app.get('/download', async (req,res)=>{
         res.send(Buffer.from(b64, 'base64'))
         console.log("File Downloaded")
         doc=null
+    } catch(err){
+        console.log("From GET: \n",err)
+        res.send("some error occured!")
+    }
+})
+
+
+// Pytesseract container
+// Request mode for pytess
+app.post('/pytess/docx',async (req,res)=>{
+    console.log("recieved Request")
+    let list=req.body.data.slice(1)
+try {     
+    pytessDocx=new docx.Document();
+    console.log("Request Body: \n",req.body)
+    
+    filename=req.body.data[0]
+    console.log('List is: \n',list)
+  
+    for(let i=0;i<list.length; i++) {
+        // console.log(list[i].split("\n"))
+      
+        pytessDocx.addSection({            
+            children: paraWrite(list[i])
+        })
+       }
+       
+console.log("List: \n",list)
+    list=null
+    console.log("List after null: \n",list)
+   res.status(200).send('File ready for download!')
+    
+}
+catch(err) {
+    console.log("Error: \n",err)
+    res.status(200).send("Error Occured!\n")
+    }    
+})// end of request
+
+app.get('/pytess/docx', async (req,res)=>{
+    try {
+        console.log('request from device for docx')
+    
+        const b64=await docx.Packer.toBase64String(pytessDocx)
+        console.log(filename,"From download route\n Downloading file...")
+        res.setHeader('Content-Disposition', "attachment; filename="+filename+".docx")
+        res.send(Buffer.from(b64, 'base64'))
+        console.log("File Downloaded")
+        pytessDocx=null
     } catch(err){
         console.log("From GET: \n",err)
         res.send("some error occured!")
